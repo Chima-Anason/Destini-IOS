0b34b950f196171c1af02796fa156c63a3321087
Chima-Anason <tblynxx@gmail.com>
2021-12-08 12:16:30 +0800

    Initial commit

---
+++ Destini-iOS13/Controller/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  Destini-iOS13
+//
+//  Created by chima on 8/20/20.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var storyLabel: UILabel!
+    @IBOutlet weak var choice1Button: UIButton!
+    @IBOutlet weak var choice2Button: UIButton!
+    
+ 
+    var storyBrain = StoryBrain()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+  
+        
+        updateUI()
+    }
+
+    @IBAction func choiceMade(_ sender: UIButton) {
+        
+        let userChoice = sender.currentTitle!
+        storyBrain.nextStory(userChoice: userChoice)
+        
+        updateUI()
+    }
+    
+    func updateUI(){
+        
+        
+        storyLabel.text = storyBrain.getStoryText()
+        
+ 
+        choice1Button.setTitle(storyBrain.getChoice1(), for: .normal)
+        
+      choice2Button.setTitle(storyBrain.getChoice2(), for: .normal)
+        
+    }
+    
+}
+
